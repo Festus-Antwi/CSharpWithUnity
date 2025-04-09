e5b2a3c3bd9723252e823809fd1886f7e76f6915
Fez <festussarfo28@gmail.com>
2025-04-09 12:42:46 +0000

    initail commit

---
+++ Assets/Scripts/Press.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using TMPro;
+public class Press : MonoBehaviour
+{
+    private TextMeshProUGUI numberLabel;
+    
+    int number;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        numberLabel= GetComponent<TextMeshProUGUI>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IncreseNumber()
+    {
+        numberLabel.text = (number += 1).ToString();
+        
+    }
+
+
+    public void DecreaseNumber()
+    {
+        numberLabel.text = (number -= 1).ToString();
+        
+    }
+
+
+    
+    public void ResetNumber()
+    {
+        numberLabel.text = (number = 0).ToString();
+        
+    }
+}
